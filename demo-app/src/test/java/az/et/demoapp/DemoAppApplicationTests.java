96f28c66d29235598ef48815486a0fabedc9ae24
Elvin Taghizade <elvin.tagizade@guavapay.com>
2022-04-24 12:11:37 +0400

    spring example attached

---
+++ demo-app/src/test/java/az/et/demoapp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.et.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
