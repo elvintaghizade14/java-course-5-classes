9776220db27a15a3459ae6cca3ebc807d633b6cb
Elvin Taghizade <elvin.tagizade@guavapay.com>
2022-05-07 11:28:32 +0400

    spring intro

---
+++ spring-tymeleaf-demo/src/main/java/az/et/springtymeleafdemo/SpringTymeleafDemoApplication.java
@@ -0,0 +1,13 @@
+package az.et.springtymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTymeleafDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTymeleafDemoApplication.class, args);
+    }
+
+}
---
+++ spring-tymeleaf-demo/src/test/java/az/et/springtymeleafdemo/SpringTymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.et.springtymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTymeleafDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
